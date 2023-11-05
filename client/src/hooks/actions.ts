cde5d1f99d42f2a6be4cc26cb0ac86ea8e7e1ef3
aeross <aerosstube@gmail.com>
2023-11-05 16:11:49 +0300

    initial commit

--- client/src/hooks/actions.ts
+++ client/src/hooks/actions.ts
@@ -1,12 +1,9 @@
-import { useDispatch } from 'react-redux';
-import { bindActionCreators } from '@reduxjs/toolkit';
+import { useDispatch } from "react-redux";
+import { bindActionCreators } from "@reduxjs/toolkit";
 
-
-const actions = {
-
-};
+const actions = {};
 
 export const useActions = () => {
-	const dispatch = useDispatch();
-	return bindActionCreators(actions, dispatch);
+  const dispatch = useDispatch();
+  return bindActionCreators(actions, dispatch);
 };
