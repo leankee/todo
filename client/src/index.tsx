05211a485f4646eb8b6534f7b2d05988dfa9cd2b
stephan_v <leankee217@gmail.com>
2023-11-08 02:22:49 +0300

    feat(font): Подключение шрифта Montserrat

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import '@fontsource/montserrat';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
