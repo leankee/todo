f5ebc2bb67df33c8eedf02cbffa660510513c10b
stephan_v <leankee217@gmail.com>
2023-11-08 02:27:12 +0300

    feat(theme): Добавление темы компонентов

---
+++ client/src/styles/theme.ts
@@ -0,0 +1,17 @@
+import { ThemeConfig } from 'antd/es/config-provider/context';
+import variables from './variables.scss';
+import { theme } from 'antd';
+
+const { darkAlgorithm } = theme;
+export const appTheme: ThemeConfig = {
+  algorithm: darkAlgorithm,
+  token: {
+    colorPrimary: variables.primary,
+    colorInfo: variables.primary,
+    colorBgBase: variables.component,
+    colorTextBase: variables.text,
+    colorSuccess: variables.success,
+    colorWarning: variables.warning,
+    colorError: variables.error,
+  },
+};
