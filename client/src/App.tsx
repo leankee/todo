16ba55a95a3bab248a1cbe64f4ea9ed63337bd55
stephan_v <leankee217@gmail.com>
2023-11-08 02:23:14 +0300

    feat(theme): Подключение темы

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,11 @@ import ru_RU from 'antd/lib/locale/ru_RU';
 import 'antd/dist/reset.css';
 import './styles/globals.scss';
 import { Router } from './hoc/Router';
+import { appTheme } from './styles/theme';
 
 function App() {
 	return (
-		<ConfigProvider locale={ru_RU}>
+		<ConfigProvider theme={appTheme} locale={ru_RU}>
 			<Router/>
 		</ConfigProvider>
 	);
