cb7151bbf01254bbcf455fcbc1eed0d4b2bab5c0
leankee <leankee217@gmail.com>
2023-11-09 23:08:48 +0300

    feat(logic): Добавление тестовой логики авторизации

--- client/src/pages/sign-in.tsx
+++ client/src/pages/sign-in.tsx
@@ -1,16 +1,27 @@
-import { Button, Form, Input, Typography } from 'antd';
+import { Button, Form, FormProps, Input, Typography } from 'antd';
 import { PageForm } from '../hoc/Page/Form';
 import { PageNames, RouteNames } from '../hoc/Router';
 import { LockOutlined, MailOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import styles from '../styles/pages/sign-in.module.scss';
+import { useActions } from '../hooks/actions';
+import { IUser } from '../models';
 
 const SignIn = () => {
+	const { login } = useActions(); // Изменить
+
+	const handleSubmit: FormProps['onFinish'] = (values) => {
+		// Изменить
+		const user: IUser = { email: values.email };
+		login(JSON.stringify(user));
+	};
+
 	return (
 		<PageForm
 			title={PageNames.SIGN_IN}
 			form={{
 				name: 'login',
+				onFinish: handleSubmit,
 			}}
 		>
 			<Form.Item
