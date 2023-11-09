d4ebd4b4f9c4224ddbdf2e5d9b5832fc2eb92773
leankee <leankee217@gmail.com>
2023-11-09 23:05:42 +0300

    feat(logic): Добавление логики маршрутов в зависимости от авторизации пользователя

--- client/src/hoc/Router.tsx
+++ client/src/hoc/Router.tsx
@@ -1,7 +1,11 @@
 import { FC, ReactNode } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes, useLocation, useSearchParams } from 'react-router-dom';
 import Home from '../pages';
 import SignIn from '../pages/sign-in';
+import { useAppSelector } from '../hooks/redux';
+import { Layout } from './Layout';
+import { ResultNotFound } from '../components/Result/NotFound';
+import { ResultDeveloping } from '../components/Result/Developing';
 
 interface IRoute {
 	path: string;
@@ -10,13 +14,23 @@ interface IRoute {
 
 export enum RouteNames {
 	HOME = '/',
+	CALENDAR = '/calendar',
+	NOTES = '/notes',
+	TASK = '/task/:task_id',
+	FAVOURITES = '/favourites',
+	GROUP = '/group/:group_id',
 	SIGN_IN = '/sign-in',
 	SIGN_UP = '/sign-up',
+	NOT_FOUND = '*',
 }
 
 export enum PageNames {
 	HOME = 'Главная',
+	CALENDAR = 'Календарь',
+	NOTES = 'Заметки',
+	FAVOURITES = 'Избранное',
 	SIGN_IN = 'Вход в аккаунт',
+	SIGN_UP = 'Регистрация',
 }
 
 const authRoutes: IRoute[] = [
@@ -24,20 +38,86 @@ const authRoutes: IRoute[] = [
 		path: RouteNames.SIGN_IN,
 		element: <SignIn/>,
 	},
+	{
+		path: RouteNames.SIGN_UP,
+		element: <ResultDeveloping/>,
+	},
 ];
 
-const publicRoutes: IRoute[] = [
+const privateRoutes: IRoute[] = [
 	{
 		path: RouteNames.HOME,
 		element: <Home/>,
 	},
+	{
+		path: RouteNames.TASK,
+		element: <ResultDeveloping/>,
+	},
+	{
+		path: RouteNames.CALENDAR,
+		element: <ResultDeveloping/>,
+	},
+	{
+		path: RouteNames.NOTES,
+		element: <ResultDeveloping/>,
+	},
+	{
+		path: RouteNames.FAVOURITES,
+		element: <ResultDeveloping/>,
+	},
+	{
+		path: RouteNames.GROUP,
+		element: <ResultDeveloping/>,
+	},
 ];
 
 export const Router: FC = () => {
+	const { data: user } = useAppSelector(state => state.user);
+	const location = useLocation();
+	const [searchParams] = useSearchParams();
+	const redirect = searchParams.get('return_to');
+
 	return (
 		<Routes>
-			{publicRoutes.map(route => <Route key={route.path} path={route.path} element={route.element}/>)}
-			{authRoutes.map(route => <Route key={route.path} path={route.path} element={route.element}/>)}
+			{user
+				? (
+					<Route path={RouteNames.HOME} element={<Layout/>}>
+						{privateRoutes.map(route => (
+							<Route
+								key={route.path}
+								path={route.path}
+								element={route.element}
+							/>
+						))}
+						{authRoutes.map(route => (
+							<Route
+								key={route.path}
+								path={route.path}
+								element={<Navigate to={redirect || RouteNames.HOME} replace/>}
+							/>
+						))}
+						<Route path={RouteNames.NOT_FOUND} element={<ResultNotFound/>}/>
+					</Route>
+				) : (
+					<>
+						{authRoutes.map(route => (
+							<Route
+								key={route.path}
+								path={route.path}
+								element={route.element}
+							/>
+						))}
+						{privateRoutes.map(route => (
+							<Route
+								key={route.path}
+								path={route.path}
+								element={<Navigate to={`${RouteNames.SIGN_IN}?return_to=${location.pathname}`} replace/>}
+							/>
+						))}
+						<Route path={RouteNames.NOT_FOUND} element={<ResultNotFound/>}/>
+					</>
+				)
+			}
 		</Routes>
 	);
 };
