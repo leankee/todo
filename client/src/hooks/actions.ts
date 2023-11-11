6ed407374b3405ce98fadc431f63b9fc0a7f5336
leankee <leankee217@gmail.com>
2023-11-12 00:32:11 +0300

    feat(actions): Добавления взаимодействий с главной страницей

--- client/src/hooks/actions.ts
+++ client/src/hooks/actions.ts
@@ -1,9 +1,11 @@
 import { useDispatch } from 'react-redux';
 import { bindActionCreators } from '@reduxjs/toolkit';
 import { userActions } from '../store/user/user.slice';
+import { homeActions } from '../store/home/home.slice';
 
 const actions = {
 	...userActions,
+	...homeActions,
 };
 
 export const useActions = () => {
