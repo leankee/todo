e68897e126fab8a2f1d9822d4a438502e4389199
leankee <leankee217@gmail.com>
2023-11-12 00:34:41 +0300

    feat(logic): Настройка dayjs

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import '@fontsource/montserrat';
+import dayjs from 'dayjs';
+import weekOfYear from 'dayjs/plugin/weekOfYear';
+import quarterOfYear from 'dayjs/plugin/quarterOfYear';
+import ru_RU from 'dayjs/locale/ru';
+
+dayjs.extend(weekOfYear);
+dayjs.extend(quarterOfYear);
+dayjs.locale(ru_RU);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
