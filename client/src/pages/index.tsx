84083968e413da3066e8cdaa83f288cb30a32362
leankee <leankee217@gmail.com>
2023-11-12 00:32:56 +0300

    fix(page): Обновление главной страницы

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,20 +1,44 @@
-import { Link } from 'react-router-dom';
-import { PageNames, RouteNames } from '../hoc/Router';
-import { Button } from 'antd';
+import { DatePicker, DatePickerProps } from 'antd';
 import { useActions } from '../hooks/actions';
 import { Page } from '../hoc/Page';
+import dayjs from 'dayjs';
+import { useAppSelector } from '../hooks/redux';
+import { dateAPIFormat, dateFormat, dateFullFormat } from '../utils';
+import { ResultDeveloping } from '../components/Result/Developing';
+import { ControlFilter } from '../components/Control/Filter';
+import { ControlView, ControlViewProps } from '../components/Control/View';
 
 const Home = () => {
-	const { logout } = useActions();
+	const { params, grid } = useAppSelector(state => state.home);
+	const { setHomeParams, setHomeGrid } = useActions();
 
-	const handleClick = () => {
-		logout();
+	const handleDateChange: DatePickerProps['onChange'] = (date) => {
+		setHomeParams({ ...params, date: dayjs(date).format(dateAPIFormat) });
+	};
+
+	const handleViewChange: ControlViewProps['onViewChange'] = (grid) => {
+		setHomeGrid(grid);
 	};
 
 	return (
-		<Page header={{ title: PageNames.HOME.toUpperCase() }}>
-			<Link to={RouteNames.SIGN_IN}>{PageNames.SIGN_IN}</Link>
-			<Button onClick={handleClick}>Выйти из аккаунта</Button>
+		<Page
+			header={{
+				title: dayjs(params.date).format(dateFullFormat).toUpperCase(),
+				controls: [
+					<ControlView grid={grid} onViewChange={handleViewChange}/>,
+					<ControlFilter/>,
+					'divider',
+					<DatePicker
+						value={dayjs(params.date)}
+						allowClear={false}
+						placement="bottomRight"
+						onChange={handleDateChange}
+						format={dateFormat}
+					/>,
+				],
+			}}
+		>
+			<ResultDeveloping/>
 		</Page>
 	);
 };
