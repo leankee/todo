b2ad67e99cb5597359bad2f11ace7193b633c192
stephan_v <leankee217@gmail.com>
2023-11-13 17:07:49 +0300

    feat(models): Добавление моделей задачи, группы и заметки

--- client/src/models/index.ts
+++ client/src/models/index.ts
@@ -1,3 +1,35 @@
 export interface IUser {
 	email: string;
 }
+
+export interface IGroup {
+	id: number;
+	name: string;
+	color: string;
+}
+
+export interface ISubtask {
+	id: number;
+	name: string;
+	isDone: boolean;
+	parentId: number;
+	date: string;
+}
+
+export interface ITask {
+	id: number;
+	name: string;
+	description?: string;
+	isDone: boolean;
+	isFavourite: boolean;
+	date: string;
+	group?: IGroup;
+	children?: ISubtask[];
+}
+
+export interface INote {
+	id: number;
+	name: string;
+	description: string;
+	color: string;
+}
