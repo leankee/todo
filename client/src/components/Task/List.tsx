3beb25304211e00bca97defebb7c3178eff3b39e
leankee <leankee217@gmail.com>
2023-11-17 01:52:45 +0300

    feat(props): Добавление пропса текущей даты

--- client/src/components/Task/List.tsx
+++ client/src/components/Task/List.tsx
@@ -6,12 +6,13 @@ import styles from '../../styles/components/task.module.scss'
 
 export interface TaskListProps {
 	tasks: ITask[];
+	currentDate?: string;
 }
 
-export const TaskList: FC<TaskListProps> = ({ tasks }) => {
+export const TaskList: FC<TaskListProps> = ({ tasks, currentDate }) => {
 	return (
 		<Flex className={styles.list} gap={8} vertical>
-			{tasks.map(task => <Task key={task.id} {...task} />)}
+			{tasks.map(task => <Task key={task.id} {...task} currentDate={currentDate} />)}
 		</Flex>
 	);
 };
