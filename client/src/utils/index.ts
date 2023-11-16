719d58e9637ecfbbdb40cb1328b59d7fd3db82db
leankee <leankee217@gmail.com>
2023-11-17 01:51:21 +0300

    feat(utils): Добавление вспомогательных функций/констант

--- client/src/utils/index.ts
+++ client/src/utils/index.ts
@@ -1,3 +1,5 @@
 export const dateFormat = 'DD.MM.YYYY';
 export const dateFullFormat = 'D MMMM YYYY';
 export const dateAPIFormat = 'YYYY-MM-DD';
+export const dateTimeFormat = 'HH:mm';
+export const dateTimeAPIFormat = 'YYYY-MM-DD HH:mm';
